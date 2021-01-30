302e634fee80dbacc5eeb500dcb004f5023f8a21
toddmartin <kj4ntv@gmail.com>
2021-01-29 23:41:37 -0800

    CREATE CLASS AND IMPLEMENTATION created the class and the basic implementation that just takes a string and returns the string as a json object

---
+++ Assignment3/src/main/java/TCP/JSONMessageParser.java
@@ -0,0 +1,12 @@
+package TCP;
+
+import java.util.Map;
+import org.json.*;
+
+public class JSONMessageParser {
+
+	public static JSONObject getJSONObject (String source) {
+		return new JSONObject(source);
+	}
+
+}
