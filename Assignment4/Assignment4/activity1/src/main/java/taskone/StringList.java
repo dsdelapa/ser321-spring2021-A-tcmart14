2a51a87bf7996264a6572ad2fcb2c7d76c9e9e38
toddmartin <kj4ntv@gmail.com>
2021-02-03 21:12:54 -0800

    IMPLEMENT POP FUNCTION
    Implements the pop function. Pop function return the last element in the array datastructure as this is ccurrently considered the "top." If the list is empty, it will return "null"

--- Assignment4/Assignment4/activity1/src/main/java/taskone/StringList.java
+++ Assignment4/Assignment4/activity1/src/main/java/taskone/StringList.java
@@ -25,6 +25,14 @@ class StringList {
     public String toString() {
         return strings.toString();
     }
+
+    public String pop () {
+        if (this.size() == 0)
+            return "null";
+
+        return strings.get(this.size()-1);
+    }
+
     public String count () {
         return Integer.toString(this.size());
     }
